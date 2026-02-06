c745b92be9cb8b87ec2a7921ffe6234f3401806d
AdityaUniyal97 <160417678+AdityaUniyal97@users.noreply.github.com>
2026-02-06 20:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (88.02%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        skips = 0
+        skipt = 0
+        i = len(s) - 1
+        j = len(t) - 1
+        while i >= 0 or j >= 0:
+            while i >= 0:
+                if s[i] == '#':
+                    skips+=1
+                    i-=1
+                elif skips > 0:
+                    skips-=1
+                    i-=1
+                else:
+                    break;
+            while j >= 0:
+                if t[j] == '#':
+                    skipt +=1
+                    j-=1
+                elif skipt > 0:
+                    skipt -= 1
+                    j -= 1
+                else:
+                    break
+            if i >= 0 and j >= 0:
+                if s[i] != t[j]:
+                    return False
+            elif i >= 0 or j >= 0:
+                return False
+            i-=1
+            j-=1
+        return True
+        
